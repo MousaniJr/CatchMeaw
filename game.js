de2a4475cc2c4b6976436442f46d48f444bd9958
MousaniJr <140167460+MousaniJr@users.noreply.github.com>
2023-07-21 13:38:05 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,19 @@
+        // Set up the game canvas and context
+        const canvas = document.getElementById('gameCanvas');
+        const ctx = canvas.getContext('2d');
+
+        // Define game variables and objects
+        let player = {
+            x: canvas.width / 2,
+            y: canvas.height - 50,
+            width: 80,
+            height: 50,
+            speed: 5
+        };
+
+        // Set Borders limit
+        const canvasLeftBoundary = 0;
+        const canvasRightBoundary = canvas.width - player.width;
+
+        let items = [];
+        let score = 0;
---
+++ images.js
@@ -0,0 +1,2 @@
+        let playerImage = new Image();
+        playerImage.src = 'player-right.png'; // Replace with the actual path to the right player image
